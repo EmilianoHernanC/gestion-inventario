e3c95d7904989a0021169255803bc35b7dd81aeb
EmilianoHernanC <emi__nirvana@hotmail.com.ar>
2025-08-10 22:45:18 -0300

    Eliminar carpeta client del repositorio

--- client/src/hooks/TranslationHook.ts
+++
@@ -1,9 +0,0 @@
-import { useStore } from "../store";
-import { Translation } from "../components/ui/Translation";
-
-
-export  function useTranslation() {
-  const lang = useStore((state) => state.language)
-  const t = Translation[lang]
-  return t
-}
--- client/src/views/Stocks.tsx
+++
@@ -1,6 +0,0 @@
-
-export default function Stocks() {
-  return (
-    <div>Stocks</div>
-  )
-}
